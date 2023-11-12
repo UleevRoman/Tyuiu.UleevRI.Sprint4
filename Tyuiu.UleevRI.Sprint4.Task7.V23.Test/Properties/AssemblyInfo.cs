65427374464a8373e973e0ef6fe74d908323d10c
Tyuiu.UleevRI@outlook.com <romanuleev178@gmail.com>
2023-11-12 18:48:21 +0500

    Task7

---
+++ Tyuiu.UleevRI.Sprint4.Task7.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.UleevRI.Sprint4.Task7.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.UleevRI.Sprint4.Task7.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("def6ce09-32f8-4603-8424-ea61633672d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.UleevRI.Sprint4.Task7.V23/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.UleevRI.Sprint4.Task7.V23.Lib;
+
+namespace Tyuiu.UleevRI.Sprint4.Task7.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 4;
+            int columns = 3;
+            int[,] matrix = new int[rows, columns];
+            string str = "678135972584";
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Улеев Р.И. | ИИПБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант 23                                                              *");
+            Console.WriteLine("* Выполнил: Улеев Роман Игоревич | ИИПБ-23-3                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '678135972584'. Преобразуйте ее в     *");
+            Console.WriteLine("* матрицу 4 на 3 и подсчитайте количество четных чисел.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int index = 0;
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index += 1;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(rows, columns, str);
+            Console.WriteLine("Количество четных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
